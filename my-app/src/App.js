de505a992180065c7383f28af060fa1022e17aa9
Louie Farol <louie.farol@sap.com>
2018-03-22 19:29:23 -0700

    Search bar working

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import SearchInput, {createFilter} from 'react-search-input'
 import logo from './logo.svg';
-import Data from './people.js';
+import data from './people.js';
 import './App.css';
 
+const KEYS_TO_FILTERS = ['name'];
+
 class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      searchTerm: ''
+    }
+    this.searchUpdated = this.searchUpdated.bind(this)
+  }
   render() {
+    const filteredData = data.filter(createFilter(this.state.searchTerm, KEYS_TO_FILTERS))
     return (
       <div className="App">
         <header className="App-header">
@@ -14,12 +25,15 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-
-        <ul>
-          {Data.map(x => <li>{x.name}</li>)}
-        </ul>
+        
+        <SearchInput className="search-input" onChange={this.searchUpdated} />
+        {filteredData.map(x => <li>{x.name}</li>)}
       </div>
     );
+  }
+
+  searchUpdated (term) {
+    this.setState({searchTerm: term})
   }
 }
 export default App;
